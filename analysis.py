62e5c17a40c22c88224f77b887011291746de6ae
Polina Golikova <98438596+polina-golikova@users.noreply.github.com>
2023-11-02 18:25:54 -0500

    working code

--- analysis.py
+++ analysis.py
@@ -1,46 +1,80 @@
-import pandas as pd
 import numpy as np
-output_file = 'myOutput.txt'
-
-fOut = open(output_file, 'w')
-test = open('testTrack_hierarchy.txt', 'r')
-train = open('trainIdx2_matrix.txt', 'r')
-
-testData = []
-trainData = []
-
-for line in test:
-    testData.append(line.strip().split("|"))
-for line in train:
-    trainData.append(line.strip().split("|"))
-
-result = []
-for i, row in enumerate(testData):
-    for j, cell in enumerate(row):
-        if cell == 'None':
-            testData[i][j] = 0
-        else:
-            testData[i][j] = int(testData[i][j])
-    result.append([testData[i][0], testData[i][1], 0])
-    
-for i, row in enumerate(trainData):
-    for j, cell in enumerate(row):
-        trainData[i][j] = int(trainData[i][j])            
+import multiprocessing
+import cProfile
+import time
+import csv
 
 
-for i, row in enumerate(trainData):
-    for j, music in enumerate(testData):
-        if trainData[i][0] == testData[j][0]:
-            if trainData[i][1] in testData[j][1:]:
-                result[j][2] += trainData[i][2]
-                break
+def prepareTrain():
+    newTrainData = np.genfromtxt('train.txt', dtype=np.int32, delimiter='|')
+    return newTrainData
 
-for row in result:
-    print(result)
-    
-for i, row in enumerate(result):
-    outStr = ""
-    outStr += str(result[i][0]) + "|" + str(result[i][1]) + "|" + str(result[i][2])
-    fOut.write(outStr + '\n')
 
-fOut.close()
+def prepareTest():
+    test = open('test.txt', 'r')
+    tData = []
+    for line in test:
+        tData.append(line.strip().split("|"))
+    result = np.zeros((len(tData), 3), dtype=np.int32)
+
+    for i, row in enumerate(tData):
+        for j, cell in enumerate(row):
+            if cell == 'None':
+                tData[i][j] = 0
+            else:
+                tData[i][j] = int(tData[i][j])
+        result[i][0] = tData[i][0]
+        result[i][1] = tData[i][1]
+
+    newTestData = np.zeros([len(tData), len(max(tData, key=lambda x: len(x)))], dtype=np.int32)
+    for i, j in enumerate(tData):
+        newTestData[i][0:len(j)] = j
+
+    return newTestData, result
+
+
+def calculateRating(training, testing, final):
+    sizeTest = len(testing)
+    maxUserID = testing[sizeTest - 1][0]
+    minUserID = testing[0][0]
+    uniqueID = np.array(list(set([row[0] for row in testing])), dtype=np.int32)
+    matchingTraining = [rating for rating in training if rating[0] in uniqueID]
+
+    matching_dict = {}
+    for rating in matchingTraining:
+        key = rating[0]
+        if key not in matching_dict:
+            matching_dict[key] = []
+        matching_dict[key].append(rating[1:])
+
+    for i, music in enumerate(testing):
+        if music[0] in matching_dict:
+            for rating in matching_dict[music[0]]:
+                if rating[0] in music[1:]:
+                    final[i][2] += rating[1]
+    final = final[np.lexsort((final[:, 2],))]
+    final = final[np.lexsort((final[:, 0],))]
+
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field = ['TrackID', 'Predictor']
+        writer.writerow(field)
+        j = 0
+        for row in final:
+            if j in (0, 1, 2):
+                s = str(row[0]) + "_" + str(row[1])
+                writer.writerow([s, 0])
+            elif j in (3, 4, 5):
+                s = str(row[0]) + "_" + str(row[1])
+                writer.writerow([s, 1])
+            j = (j + 1) % 6
+
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    trainData = prepareTrain()
+    testData, finalRating = prepareTest()
+    calculateRating(trainData, testData, finalRating)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    print(total_time / 60)
